cf0771e7d9163cb5df32d7098890caae21252e1c
PMencos <108150574+PMencos@users.noreply.github.com>
2022-07-12 15:13:28 -0400

    Add files via upload

--- Paul-Branch.sql
+++ Paul-Branch.sql
@@ -1,7 +1,7 @@
 -- Table structure for table `certifications`
 
 CREATE TABLE Certifications(
-certid SMALLINT UNSIGNED NOT NULL,
+certid SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 emplid SMALLINT UNSIGNED NOT NULL,
 Cloud_Architect BOOLEAN NOT NULL,
 Cloud_Architect_Experation DATE,
@@ -19,13 +19,13 @@ HRCI_Certification BOOLEAN NOT NULL,
 HRCI_Certification_Experation DATE,
 SHRM_Certification BOOLEAN NOT NULL,
 SHRM_Certification_Experation DATE,
-PRIMARY KEY (certid))
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+PRIMARY KEY (certid)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `training`
 
 CREATE TABLE Training(
-trainid SMALLINT UNSIGNED NOT NULL,
+trainid SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 emplid SMALLINT UNSIGNED NOT NULL,
 cybersec BOOLEAN NOT NULL,
 cybersecdate DATE,
@@ -35,13 +35,13 @@ companyculture BOOLEAN NOT NULL,
 companyculturedate DATE,
 intellectualproperty BOOLEAN NOT NULL,
 intellectualpropertydate DATE,
-PRIMARY KEY (trainid))
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+PRIMARY KEY (trainid)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `qualifications`
 
 CREATE TABLE Qualifications(
-qualid SMALLINT UNSIGNED NOT NULL,
+qualid SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 emplid SMALLINT UNSIGNED NOT NULL,
 associates_degree BOOLEAN NOT NULL,
 associates_degree_school VARCHAR(100) NOT NULL,
@@ -54,13 +54,13 @@ masters_degree_school VARCHAR(100) NOT NULL,
 masters_degree_date DATE,
 experience INT NOT NULL,
 language_ability BOOLEAN NOT NULL,
-PRIMARY KEY (qualid))
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+PRIMARY KEY (qualid)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `applicant tracking`
 
 CREATE TABLE Applicant_Tracking(
-appid SMALLINT UNSIGNED NOT NULL,
+appid SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
 app_fname VARCHAR(50) NOT NULL,
 app_lname VARCHAR(50) NOT NULL,
 app_email VARCHAR(50) NOT NULL,
@@ -77,8 +77,8 @@ interviewer_notes VARCHAR(100) NOT NULL,
 app_phase3 BOOLEAN NOT NULL,
 app_hired BOOLEAN NOT NULL,
 app_hired_date DATE,
-PRIMARY KEY (appid))
-ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+PRIMARY KEY (appid)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- table alterations to add foreign keys
 
