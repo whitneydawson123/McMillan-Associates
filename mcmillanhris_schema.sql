713b56baa3b1928b1ab91dfb344be358d763965a
AHodsonTCC <83981267+AHodsonTCC@users.noreply.github.com>
2022-07-12 10:34:00 -0500

    Update mcmillanhris_schema.sql
    updated 'hired' to 'employment_status' in employee table. added primary key to state tax, federal tax, benefits, and payroll

--- mcmillanhris_schema.sql
+++ mcmillanhris_schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employee(
   state VARCHAR(2) NOT NULL,
   zip VARCHAR(10) NOT NULL,
   mgrid SMALLINT UNSIGNED NOT NULL,
-  hired BOOLEAN,
+  employment_status VARCHAR(45) NOT NULL,
   qualid SMALLINT UNSIGNED NOT NULL,
   certid SMALLINT UNSIGNED NOT NULL,
   jobid SMALLINT UNSIGNED NOT NULL,
@@ -36,9 +36,11 @@ CREATE TABLE employee(
 --
 
 CREATE TABLE state_tax(
+statetaxid SMALLINT UNSIGNED NOT NULL,
 emplid SMALLINT UNSIGNED NOT NULL,
 required BOOLEAN,
 rate DOUBLE NOT NULL,
+PRIMARY KEY  (statetaxid),
 FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -47,9 +49,11 @@ FOREIGN KEY (emplid) REFERENCES employee(emplid)
 --
 
 CREATE TABLE federal_tax(
+fedtaxid SMALLINT UNSIGNED NOT NULL,
 emplid SMALLINT UNSIGNED NOT NULL,
 bracket VARCHAR(10) NOT NULL,
 rate DOUBLE NOT NULL,
+PRIMARY KEY  (fedtaxid),
 FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -58,12 +62,14 @@ FOREIGN KEY (emplid) REFERENCES employee(emplid)
 --
 
 CREATE TABLE payroll(
+payrollid SMALLINT UNSIGNED NOT NULL,
 emplid SMALLINT UNSIGNED NOT NULL,
 rates DOUBLE NOT NULL,
 rates_overtime DOUBLE NOT NULL,
 totalhours SMALLINT NOT NULL,
 totalovertime SMALLINT NOT NULL,
 grosspay DOUBLE NOT NULL,
+PRIMARY KEY  (payrollid),
 FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -73,6 +79,7 @@ FOREIGN KEY (emplid) REFERENCES employee(emplid)
 --
 
 CREATE TABLE benefits(
+benid SMALLINT UNSIGNED NOT NULL,
 emplid SMALLINT UNSIGNED NOT NULL,
 healthcare VARCHAR(45) NOT NULL,
 dentalcare VARCHAR(45) NOT NULL,
@@ -81,6 +88,7 @@ annual_vacationdays SMALLINT NOT NULL,
 pension DOUBLE NOT NULL,
 ira VARCHAR(45) NOT NULL,
 maternityleave VARCHAR(45) NOT NULL,
+PRIMARY KEY  (benid),
 FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
