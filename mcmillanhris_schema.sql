308729847ff98b3b7f4a118b16f385ec66b8f5c6
AHodsonTCC <83981267+AHodsonTCC@users.noreply.github.com>
2022-07-12 10:51:34 -0500

    Update mcmillanhris_schema.sql

--- mcmillanhris_schema.sql
+++ mcmillanhris_schema.sql
@@ -92,3 +92,42 @@ PRIMARY KEY  (benid),
 FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE departments(
+	depid INT NOT NULL AUTO_INCREMENT,
+    IT BOOLEAN NOT NULL,
+    Marketing BOOLEAN NOT NULL,
+    Finance BOOLEAN NOT NULL,
+    HR BOOLEAN NOT NULL,
+    PRIMARY KEY (depid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE manager(
+	mgrid INT NOT NULL AUTO_INCREMENT,
+    depid INT NOT NULL,
+    IT_Manager BOOLEAN NOT NULL,
+    Marketing_Manager BOOLEAN NOT NULL,
+    Finance_Manager BOOLEAN NOT NULL,
+    HR_Manager BOOLEAN NOT NULL,
+    PRIMARY KEY (mgrid),
+    FOREIGN KEY (depid) REFERENCES departments (depid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE location(
+	locid INT NOT NULL AUTO_INCREMENT,
+    Minnesota_MN BOOLEAN NOT NULL,
+    Charlotte_NC BOOLEAN NOT NULL,
+    Tampa_FL BOOLEAN NOT NULL,
+    PRIMARY KEY (locid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE job(
+	jobid INT NOT NULL AUTO_INCREMENT,
+    net_developer BOOLEAN NOT NULL,
+    front_end_developer BOOLEAN NOT NULL,
+    hr_expert BOOLEAN NOT NULL,
+    marketing BOOLEAN NOT NULL,
+    accounting BOOLEAN NOT NULL,
+    back_end_developer BOOLEAN NOT NULL,
+    PRIMARY KEY(jobid),
+    FOREIGN KEY (depid) REFERENCES departments (depid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
