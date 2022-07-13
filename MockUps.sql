61d5759d473c6180f7b491ff8120749f75e6dc83
whitneydawson123 <94804621+whitneydawson123@users.noreply.github.com>
2022-07-13 15:42:11 -0400

    Add files via upload

---
+++ MockUps.sql
@@ -0,0 +1,59 @@
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Sarah', 'Brown', '77 High Street', 'Minneapolis', 'MN', '56792', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Penny', 'Smart', '99 12th Street', 'Tampa', 'FL', '56792', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Andrew', 'Nimble', '776 Willow Street', 'Tampa', 'FL', '56793', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status )
+VALUES('Terry', 'Gatlin', '555 Main Street', 'Charlotte', 'NC', '32978', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Martin', 'Finefield', '42 N Southeast Street', 'Charlotte', 'NC', '32975', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Rueben', 'Flack', '36 N Cumberland Street', 'Charlotte', 'NC', '32978', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Curtis', 'Stack', '47 Market Street', 'Minneapolis', 'MN', '47965', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Conor', 'Phalegn', '35 Maple Street', 'Minneapolis', 'MN', '47966', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Paul', 'Gray', '88 Pine Street', 'Tampa', 'FL', '56794', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Richard', 'Robinson', '96 Jefferson Street', 'Minneapolis', 'MN', '47966', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Adam', 'Huxley', '87 Summit Ave', 'Charlotte', 'NC', '32986', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Adam', 'Reign', '110 Liberty Ave', 'Tampa', 'FL', '56794', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Ryan', 'Huntsman', '120 Liberty Ave', 'Tampa', 'FL', '56794', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Ruth', 'Chestfield', '150 Jeffersonville Ave', 'Charlotte', 'NC', '32986', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Abagail', 'West', '18 Chestnut Ave', 'Charlotte', 'NC', '32984', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Hope', 'Lansidy', '15 Green Street', 'Minneapolis', 'MN', '47966', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Hazel', 'Wright', '145 Green Street', 'Minneapolis', 'MN', '47966', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Ava', 'Smith', '256 Broad Street', 'Tampa', 'FL', '56794', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Harper', 'Langley', '395 Pearl Street', 'Charlotte', 'NC', '32984', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, employment_status)
+VALUES('Chloe', 'Hansley', '567 Mill Street', 'Charlotte', 'NC', '32984', 'hired');
