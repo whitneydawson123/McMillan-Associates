b687f7a965395531e961732de0bbf741ed5923a0
AHodsonTCC <83981267+AHodsonTCC@users.noreply.github.com>
2022-07-18 08:02:37 -0500

    Added mainDisplay method to MainMenu class

--- src/main/java/com/MainMenu.java
+++ src/main/java/com/MainMenu.java
@@ -1,7 +1,19 @@
 package com;
 
 public class MainMenu {
+
+    static void mainDisplay(){
+        System.out.println("Welcome to the McMillanHRIS employee management system. " +
+                "Enter the corresponding number key to select an option.\n\n" +
+                "1. Employee information\n" +
+                "2. Applicant information\n" +
+                "3. Payroll information\n" +
+                "4. Benefits\n" +
+                "5. Evaluations\n" +
+                "6. Employee self service\n" +
+                "7. Exit system\n");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello McMillanHRIS!");
+        mainDisplay();
     }
 }
