783a2b195664ca5b7307795f22789ae4bc96b865
whitneydawson123 <94804621+whitneydawson123@users.noreply.github.com>
2022-07-21 13:45:48 -0400

    Add files via upload

---
+++ employees.sql
@@ -0,0 +1,59 @@
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Sarah', 'Brown', '77 High Street', 'Minneapolis', 'MN', '56792', 'sarahbrown@gmail.com', '123-456-7890', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Penny', 'Smart', '99 12th Street', 'Tampa', 'FL', '56792', 'pennysmart@gmail.com', '234-567-8901', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Andrew', 'Nimble', '776 Willow Street', 'Tampa', 'FL', '56793', 'andrewnimble@gmail.com', '345-678-9012', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Terry', 'Gatlin', '555 Main Street', 'Charlotte', 'NC', '32978', 'terrygatlin@gmail.com', '456-789-0123', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Martin', 'Finefield', '42 N Southeast Street', 'Charlotte', 'NC', '32975', 'martinfinefield@gmail.com', '567-890-1234', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Rueben', 'Flack', '36 N Cumberland Street', 'Charlotte', 'NC', '32978', 'ruebenflack@gmail.com', '678-901-2345', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Curtis', 'Stack', '47 Market Street', 'Minneapolis', 'MN', '47965', 'curtisstack@gmail.com', '789-012-3456', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Conor', 'Phalegn', '35 Maple Street', 'Minneapolis', 'MN', '47966', 'conorphalegan@gmail.com', '890-123-4567', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Paul', 'Gray', '88 Pine Street', 'Tampa', 'FL', '56794', 'paulgray@gmail.com', '901-234-5678', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Richard', 'Robinson', '96 Jefferson Street', 'Minneapolis', 'MN', '47966', 'richardrobinson@gmail.com', '012-345-6789', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Adam', 'Huxley', '87 Summit Ave', 'Charlotte', 'NC', '32986', 'adamhuxley@gmail.com', '098-765-4321', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Adam', 'Reign', '110 Liberty Ave', 'Tampa', 'FL', '56794', 'adamreign@gmail.com', '438-898-8580', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Ryan', 'Huntsman', '120 Liberty Ave', 'Tampa', 'FL', '56794', 'ryanhuntsman@gmail.com', '597-285-9653', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Ruth', 'Chestfield', '150 Jeffersonville Ave', 'Charlotte', 'NC', '32986', 'ruthchestfield@gmail.com', '380-570-6840', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Abagail', 'West', '18 Chestnut Ave', 'Charlotte', 'NC', '32984', 'abagailwest@gmail.com', '475-858-3685', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Hope', 'Lansidy', '15 Green Street', 'Minneapolis', 'MN', '47966', 'hopelansidy@gmail.com', '483-975-7359', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Hazel', 'Wright', '145 Green Street', 'Minneapolis', 'MN', '47966', 'hazelwright@gmail.com', '468-720-8508', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Ava', 'Smith', '256 Broad Street', 'Tampa', 'FL', '56794', 'avasmith@gmail.com', '486-839-5729', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Harper', 'Langley', '395 Pearl Street', 'Charlotte', 'NC', '32984', 'harperlangley@gmail.com', '497-868-8357', 'hired');
+
+INSERT INTO mcmillanhris.employee(fname, lname, address, city, state, zip, email, phone, employment_status)
+VALUES('Chloe', 'Hansley', '567 Mill Street', 'Charlotte', 'NC', '32984', 'chloehansley@gmail.com', '597-808-9749', 'hired');
