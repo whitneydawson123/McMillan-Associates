a905df77094736262d6bd8d27192cde223f87897
AHodsonTCC <83981267+AHodsonTCC@users.noreply.github.com>
2022-07-22 14:04:07 -0500

    Update mcmillanhris_schema.sql
    Refactored database, ALTER statements removed entirely in favor of including foreign key references in creation statements.

--- mcmillanhris_schema.sql
+++ mcmillanhris_schema.sql
@@ -14,12 +14,9 @@ CREATE TABLE employee(
   city VARCHAR(45) NOT NULL,
   state VARCHAR(2) NOT NULL,
   zip VARCHAR(10) NOT NULL,
-  mgrid INT UNSIGNED,
+  email VARCHAR(50) NOT NULL,
+  phone VARCHAR(15) NOT NULL,
   employment_status VARCHAR(45) NOT NULL,
-  qualid INT UNSIGNED,
-  certid INT UNSIGNED,
-  jobid INT UNSIGNED,
-  trainid INT UNSIGNED,
   PRIMARY KEY  (emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -29,10 +26,11 @@ CREATE TABLE employee(
 
 CREATE TABLE state_tax(
 statetaxid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
+emplid INT UNSIGNED NOT NULL,
 required BOOLEAN,
 rate DOUBLE NOT NULL,
-PRIMARY KEY  (statetaxid)
+PRIMARY KEY  (statetaxid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -41,10 +39,11 @@ PRIMARY KEY  (statetaxid)
 
 CREATE TABLE federal_tax(
 fedtaxid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
+emplid INT UNSIGNED NOT NULL,
 bracket VARCHAR(10) NOT NULL,
 rate DOUBLE NOT NULL,
-PRIMARY KEY  (fedtaxid)
+PRIMARY KEY  (fedtaxid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -52,14 +51,16 @@ PRIMARY KEY  (fedtaxid)
 --
 
 CREATE TABLE payroll(
-payrollid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
+payid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+emplid INT UNSIGNED NOT NULL,
 rates DOUBLE NOT NULL,
 rates_overtime DOUBLE NOT NULL,
 totalhours INT NOT NULL,
 totalovertime INT NOT NULL,
 grosspay DOUBLE NOT NULL,
-PRIMARY KEY  (payrollid)
+period DATE,
+PRIMARY KEY  (payid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
@@ -69,205 +70,125 @@ PRIMARY KEY  (payrollid)
 
 CREATE TABLE benefits(
 benid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
+emplid INT UNSIGNED NOT NULL,
 healthcare VARCHAR(45) NOT NULL,
 dentalcare VARCHAR(45) NOT NULL,
-annual_sickdays SMALLINT NOT NULL,
-annual_vacationdays SMALLINT NOT NULL,
+annual_vacationdays INT NOT NULL,
+annual_sickdays INT NOT NULL,
 pension DOUBLE NOT NULL,
 ira VARCHAR(45) NOT NULL,
 maternityleave VARCHAR(45) NOT NULL,
-PRIMARY KEY  (benid)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Table structure for table 'departments'
-
-CREATE TABLE departments(
-    depid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    IT BOOLEAN NOT NULL,
-    Marketing BOOLEAN NOT NULL,
-    Finance BOOLEAN NOT NULL,
-    HR BOOLEAN NOT NULL,
-    PRIMARY KEY (depid)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- Table structure for table 'manager'
-
-CREATE TABLE manager(
-    mgrid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    depid INT UNSIGNED,
-    IT_Manager BOOLEAN NOT NULL,
-    Marketing_Manager BOOLEAN NOT NULL,
-    Finance_Manager BOOLEAN NOT NULL,
-    HR_Manager BOOLEAN NOT NULL,
-    PRIMARY KEY (mgrid)
+PRIMARY KEY  (benid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table 'location'
 
 CREATE TABLE location(
-    locid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    Minnesota_MN BOOLEAN NOT NULL,
-    Charlotte_NC BOOLEAN NOT NULL,
-    Tampa_FL BOOLEAN NOT NULL,
+	locid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    address VARCHAR (50) NOT NULL,
+    city VARCHAR (50) NOT NULL,
+    state VARCHAR (50) NOT NULL,
     PRIMARY KEY (locid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table 'departments'
+
+CREATE TABLE departments(
+	depid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    locid INT UNSIGNED NOT NULL,
+    title VARCHAR (45) NOT NULL,
+    employeecount INT NOT NULL,
+    PRIMARY KEY (depid),
+    FOREIGN KEY (locid) REFERENCES location(locid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table 'job'
 
 CREATE TABLE job(
-    jobid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    depid INT UNSIGNED,
-    net_developer BOOLEAN NOT NULL,
-    front_end_developer BOOLEAN NOT NULL,
-    hr_expert BOOLEAN NOT NULL,
-    marketing BOOLEAN NOT NULL,
-    accounting BOOLEAN NOT NULL,
-    back_end_developer BOOLEAN NOT NULL,
-    PRIMARY KEY(jobid)
+	jobid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	depid INT UNSIGNED NOT NULL,
+    emplid INT UNSIGNED NOT NULL,
+    title VARCHAR (50) NOT NULL,
+    started DATE,
+    ended DATE,
+    PRIMARY KEY(jobid),
+    FOREIGN KEY (emplid) REFERENCES employee(emplid),
+    FOREIGN KEY (depid) REFERENCES departments(depid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `certifications`
 
 CREATE TABLE certifications(
 certid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
-appid INT UNSIGNED,	
-Cloud_Architect BOOLEAN NOT NULL,
-Cloud_Architect_Experation DATE,
-Solutions_Architect BOOLEAN NOT NULL,
-Solutions_Architect_Experation DATE,
-Attribution_Testing BOOLEAN NOT NULL,
-Attribution_Testing_Experation DATE,
-Automation_Artificial BOOLEAN NOT NULL,
-Automation_Artificial_Experation DATE,
-Financial_Planner BOOLEAN NOT NULL,
-Financial_Planner_Experation DATE,
-Financial_Consultant BOOLEAN NOT NULL,
-Financial_Consultant_Experation DATE,
-HRCI_Certification BOOLEAN NOT NULL,
-HRCI_Certification_Experation DATE,
-SHRM_Certification BOOLEAN NOT NULL,
-SHRM_Certification_Experation DATE,
-PRIMARY KEY (certid)
+emplid INT UNSIGNED NOT NULL,
+cert_type VARCHAR (100) NOT NULL,
+experation DATE,
+PRIMARY KEY (certid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `training`
 
 CREATE TABLE training(
 trainid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
-cybersec BOOLEAN NOT NULL,
-cybersecdate DATE,
-harrassment BOOLEAN NOT NULL,
-harassmentdate DATE,
-companyculture BOOLEAN NOT NULL,
-companyculturedate DATE,
-intellectualproperty BOOLEAN NOT NULL,
-intellectualpropertydate DATE,
-PRIMARY KEY (trainid)
+emplid INT UNSIGNED NOT NULL,
+training_type VARCHAR (100) NOT NULL,
+date_completed DATE,
+PRIMARY KEY (trainid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `qualifications`
 
 CREATE TABLE qualifications(
 qualid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
-appid INT UNSIGNED,
-associates_degree BOOLEAN NOT NULL,
-associates_degree_school VARCHAR(100) NOT NULL,
-associates_degree_date DATE,
-bachelors_degree BOOLEAN NOT NULL,
-bachelors_degree_school VARCHAR(100) NOT NULL,
-bachelors_degree_date DATE,
-masters_degree BOOLEAN NOT NULL,
-masters_degree_school VARCHAR(100) NOT NULL,
-masters_degree_date DATE,
-experience INT NOT NULL,
-language_ability BOOLEAN NOT NULL,
-PRIMARY KEY (qualid)
+emplid INT UNSIGNED NOT NULL,
+qual_type VARCHAR (100) NOT NULL,
+institution VARCHAR(50) NOT NULL,
+qual_year DATE,
+PRIMARY KEY (qualid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `applicant tracking`
 
 CREATE TABLE applicant_tracking(
 appid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-app_fname VARCHAR(50) NOT NULL,
-app_lname VARCHAR(50) NOT NULL,
-app_email VARCHAR(50) NOT NULL,
-app_phase1 BOOLEAN NOT NULL,
-app_submission BOOLEAN NOT NULL,
-app_submission_date DATE,
-app_resume BOOLEAN NOT NULL,
-qualid INT UNSIGNED NOT NULL,
-certid INT UNSIGNED NOT NULL,
-app_phase2 BOOLEAN NOT NULL,
-app_interview BOOLEAN NOT NULL,
-app_interview_date DATE,
-interviewer_notes VARCHAR(100) NOT NULL,
-app_phase3 BOOLEAN NOT NULL,
-app_hired BOOLEAN NOT NULL,
-app_hired_date DATE,
-PRIMARY KEY (appid)
+emplid INT UNSIGNED NOT NULL,
+date_applied DATE,
+date_hired DATE,
+PRIMARY KEY (appid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table 'application stage'
+
+CREATE TABLE application_stage(
+appstageid INT UNSIGNED NOT NULL AUTO_INCREMENT,
+appid INT UNSIGNED NOT NULL,
+app_stage VARCHAR (50) NOT NULL,
+started DATE,
+ended DATE,
+interview_notes VARCHAR (50) NOT NULL,
+passed BOOLEAN,
+PRIMARY KEY  (appstageid),
+FOREIGN KEY (appid) REFERENCES applicant_tracking(appid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Table structure for table 'Evaluation'
 
 CREATE TABLE evaluation(
 evalid INT UNSIGNED NOT NULL AUTO_INCREMENT,
-emplid INT UNSIGNED,
+emplid INT UNSIGNED NOT NULL,
 communication INT NOT NULL,
 job_knowledge INT NOT NULL,
 punctuality INT NOT NULL,
-depentability INT NOT NULL,
+dependability INT NULL,
 overall INT NOT NULL,
-evaluator VARCHAR(50) NOT NULL,
-mgrid INT UNSIGNED,
-datewritten date NOT NULL,
-comment VARCHAR(100),
-PRIMARY KEY (evalid)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- table alterations to add foreign keys
-
-ALTER TABLE employee
-ADD FOREIGN KEY (mgrid) REFERENCES manager(mgrid),
-ADD FOREIGN KEY (jobid) REFERENCES job(jobid),
-ADD FOREIGN KEY (qualid) REFERENCES qualifications(qualid),
-ADD FOREIGN KEY (certid) REFERENCES certifications(certid),
-ADD FOREIGN KEY (trainid) REFERENCES training(trainid);
-
-ALTER TABLE state_tax
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid);
-
-ALTER TABLE federal_tax
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid);
-
-ALTER TABLE payroll
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid);
-
-ALTER TABLE benefits
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid);
-
-ALTER TABLE manager
-ADD FOREIGN KEY (depid) REFERENCES departments(depid);
-
-ALTER TABLE job
-ADD FOREIGN KEY (depid) REFERENCES departments(depid);
-
-ALTER TABLE certifications
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid),
-ADD FOREIGN KEY (appid) REFERENCES applicant_tracking(appid);
-
-ALTER TABLE training
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid);
-
-ALTER TABLE qualifications
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid),
-ADD FOREIGN KEY (appid) REFERENCES applicant_tracking(appid);
-
-ALTER TABLE applicant_tracking
-ADD FOREIGN KEY (qualid) REFERENCES qualifications(qualid),
-ADD FOREIGN KEY (certid) REFERENCES certifications(certid);
-
-ALTER TABLE evaluation
-ADD FOREIGN KEY (emplid) REFERENCES employee(emplid),
-ADD FOREIGN KEY (mgrid) REFERENCES manager(mgrid);
+evaluator VARCHAR (50) NOT NULL,
+date_written DATE,
+comments VARCHAR (50) NOT NULL,
+PRIMARY KEY (evalid),
+FOREIGN KEY (emplid) REFERENCES employee(emplid)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
